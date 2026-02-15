4f3183e7495bb94e2f01ea14f18ad736600db028
Kipkorir Cheruiyot <154062139+ArapCheruiyot@users.noreply.github.com>
2026-02-15 02:42:40 -0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,12 @@
 # gunicorn.conf.py
 bind = "0.0.0.0:10000"
-workers = 2
+workers = 1
 timeout = 120
 keepalive = 5
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
+max_requests = 50
+max_requests_jitter = 10
+worker_class = "sync"
+worker_connections = 1000
+limit_request_line = 4094
+limit_request_fields = 100
+limit_request_field_size = 8190
